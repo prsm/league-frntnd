69ed924267186974f4d97758cde5c6f8e865c56e
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2020-10-01 10:42:11 +0200

    :sparkles: finished starting page

--- components/WelcomePage.js
+++ components/WelcomePage.js
@@ -8,9 +8,9 @@ export default function WelcomePage() {
   return (
     <>
       <div className="w-full bg-prsm-dark-700 overflow-hidden relative">
-        <div className=" mt-8 mb-2 mx-4 px-4 py-4 lg:px-64 lg:py-20">
-          <FontLogo className="text-white fill-current w-32 lg:w-48" />
-          <h1 className="text-white font-bold text-4xl mt-4 lg:text-6xl lg:mb-8">
+        <div className=" mt-8 mb-2 mx-4 px-4 py-4 lg:px-32 xl:px-64 lg:py-20">
+          <FontLogo className="text-white fill-current w-32 lg:w-48 float-right " />
+          <h1 className="text-white font-bold text-4xl mt-16 lg:text-6xl lg:mb-8">
             LEAGUE<span className=" text-prsm-red-500 text-sm lg:text-base align-top">BETA</span>
           </h1>
           <p className="text-white font-medium text-lg">
@@ -26,12 +26,12 @@ export default function WelcomePage() {
         <Logo className="block -z-10 absolute" />
       </div>
       <div className="flex flex-col lg:flex-row lg:justify-between">
-        <div className="mt-6 mb-2 mx-4 pt-2 pb-4 px-4 bg-prsm-dark-700 rounded-md shadow-lg lg:w-1/2">
+        <div className="mt-6 mb-2 mx-4 pt-2 pb-4 px-4 bg-prsm-dark-700 rounded-md shadow-lg lg:w-1/2 lg:p-8">
           <h2 className=" text-white font-bold text-2xl text-center">Enter the LEAGUE</h2>
           <p className="text-white text-center mb-4 font-medium">Battle it out against other PR1SM members</p>
           <LoginButton />
         </div>
-        <div className="mt-6 mb-2 mx-4 pt-2 pb-4 px-4 bg-prsm-dark-700 rounded-md shadow-lg lg:w-1/2">
+        <div className="mt-6 mb-2 mx-4 pt-2 pb-4 px-4 bg-prsm-dark-700 rounded-md shadow-lg lg:w-1/2 lg:p-8">
           <h2 className=" text-white font-bold text-2xl text-center">Get the latest</h2>
           <p className="text-white text-center mb-4 font-medium">Join our Discord server to see upcoming matches</p>
           <motion.a
@@ -45,38 +45,46 @@ export default function WelcomePage() {
           </motion.a>
         </div>
       </div>
-      <div className="bg-prsm-dark-700 rounded-md shadow-lg mt-4 mb-2 py-4 px-4 mx-4">
-        <h2 className="text-white font-bold text-2xl mb-4 ">
-          CoD Olympics<span className=" text-prsm-red-500 text-xs align-top">Next event</span>
+      <div className="bg-prsm-dark-700 rounded-md shadow-lg mt-4 mb-6 py-4 px-4 mx-4 lg:p-16">
+        <h2 className="text-white font-bold text-2xl lg:text-3xl mb-4 ">
+          CoD Olympics<span className=" text-prsm-red-500 text-xs align-top lg:text-base">Next event</span>
         </h2>
-        <img className="rounded-md  border-white border-2 mb-4" src="banner.png" alt="PR1SM CoD Olympics banner" />
-        <p className="text-white font-medium text-lg mt-0">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt efficitur quam, ac convallis lectus molestie id. Ut imperdiet ipsum nec
-          turpis pretium lacinia. Sed vitae pretium diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-        </p>
+        <div className="flex flex-col lg:flex-row flex-no-wrap justify-between">
+          <div className="lg:pr-4">
+            <img className="rounded-md border-white border-2 mb-4" src="banner.png" alt="PR1SM CoD Olympics banner" />
+          </div>
+          <div className="lg:ml-4">
+            <h3 className="text-white font-bold text-xl mb-4">What are the Call of Duty Olympics?</h3>
+            <p className="text-white font-medium text-lg">
+              Over the next two LAN-Events there will be different gamemodes in different games. There will be more information on this in the following days.
+              <br />
+              <span className="text-prsm-red-500">Join our Discord server to stay updated</span>
+            </p>
+            <h3 className="text-white font-bold text-xl my-4">Olympic disciplines:</h3>
+            <ul className="list-disc list-inside mb-4">
+              <li className="text-prsm-red-600">
+                <span className="text-white font-medium text-lg">LAN2K20</span>
+                <ul className="list-disc list-inside">
+                  <li className="text-prsm-red-600 ml-4">
+                    <span className="text-white font-medium text-lg">IW4x -> MW2</span>
+                  </li>
+                  <li className="text-prsm-red-600 ml-4">
+                    <span className="text-white font-medium text-lg">Plutonium -> BO2</span>
+                  </li>
+                </ul>
+              </li>
+              <li className="text-prsm-red-600">
+                <span className="text-white font-medium text-lg">Isle of Lan</span>
+                <ul className="list-disc list-inside">
+                  <li className="text-prsm-red-600 ml-4">
+                    <span className="text-white font-medium text-lg">TBD</span>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <h3 className="text-prsm-dark-800 font-bold text-xl my-2">Olympic disciplines:</h3>
-      <ul className="list-disc list-inside mb-4">
-        <li className="text-prsm-red-600">
-          <span className="text-prsm-dark-800 font-medium text-lg">Call of Duty 4: Modern Warfare</span>
-        </li>
-        <li className="text-prsm-red-600">
-          <span className="text-prsm-dark-800 font-medium text-lg">Call of Duty 4: Modern Warfare</span>
-        </li>
-        <li className="text-prsm-red-600">
-          <span className="text-prsm-dark-800 font-medium text-lg">Call of Duty 4: Modern Warfare</span>
-        </li>
-        <li className="text-prsm-red-600">
-          <span className="text-prsm-dark-800 font-medium text-lg">Call of Duty 4: Modern Warfare</span>
-        </li>
-        <li className="text-prsm-red-600">
-          <span className="text-prsm-dark-800 font-medium text-lg">Call of Duty 4: Modern Warfare</span>
-        </li>
-        <li className="text-prsm-red-600">
-          <span className="text-prsm-dark-800 font-medium text-lg">Call of Duty 4: Modern Warfare</span>
-        </li>
-      </ul>
-
       <div className="static left-0 bottom-0  w-full bg-prsm-dark-700 py-4">
         <Logo className="fill-current text-white w-12 h-12 m-auto" />
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,26 @@ import WelcomePage from '@components/WelcomePage';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useAuth } from '../contexts/auth';
+import Logo from '../public/pr1sm.svg';
 
 export default function Home() {
   const user = undefined;
   const router = useRouter();
 
-  const { isAuthenticated, saveJwt } = useAuth();
+  const { isAuthenticated, saveJwt, loading } = useAuth();
 
   const jwtCheck = () => {
     if (router.query.jwt) {
       saveJwt();
     }
   };
+
+  const loadingImage = (
+    <div className="m-auto mt-40">
+      <Logo className=" w-48 m-auto fill-current text-prsm-dark-700" />
+      <p className="text-lg font-semibold text-prsm-dark-700 text-center">loading...</p>
+    </div>
+  );
 
   https: return (
     <div>
@@ -23,7 +31,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {jwtCheck()}
-      {isAuthenticated ? <Content /> : <WelcomePage />}
+      {loading ? loadingImage : isAuthenticated ? <Content /> : <WelcomePage />}
     </div>
   );
 }
